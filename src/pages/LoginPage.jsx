eff70f8e78c60717ef912dda736dd3a1a3ff9f61
Caio Neme <caiocesarneme@gmail.com>
2023-08-15 11:51:50 -0300

    style:LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,130 @@
 import React from 'react'
+import styled from "styled-components";
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <Conteiner>
+      <ContainerText>
+        <div>
+          <h1>linkr</h1>
+          <h2>save, share and discover the best links on the web</h2>
+        </div>
+      </ContainerText>
+      <ContainerForm>
+        <form onSubmit={() => { }}>
+          <input placeholder='e-mail' type="text" />
+          <input placeholder='password' type="password" />
+          <button>Log In</button>
+        </form>
+        <p>First time? Create an account!</p>
+      </ContainerForm>
+    </Conteiner>
   )
 }
+
+const ContainerText = styled.div`
+  width:63%;
+  background-color:#151515;
+  height:100vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+
+  h1{
+    color: #FFF;
+    font-family: Passion One;
+    font-size: 106px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 5.3px;
+  }
+  h2{
+    color: #FFF;
+    font-family: Oswald;
+    font-size: 43px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    width:55%;
+  }
+`
+const ContainerForm = styled.div`
+  width:37%;
+  background-color:#333333;
+  height:100vh;
+
+  display:flex;
+  flex-direction:column;
+
+  justify-content: center;
+  align-items: center;
+  form{
+    display:flex;
+    flex-direction:column;
+
+    justify-content: center;
+    align-items: center;
+
+    input{
+      width: 429px;
+      height: 65px;
+      flex-shrink: 0;
+
+      border-radius:6px;
+      margin:8px;
+
+      color: #9F9F9F;
+      font-family: Oswald;
+      font-size: 27px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+
+      padding-left:8px;
+    }
+
+    button{
+      width: 443px;
+      height: 65px;
+      flex-shrink: 0;
+
+      border-radius: 6px;
+      background: #1877F2;
+
+      color: #FFF;
+      font-family: Oswald;
+      font-size: 27px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+
+      margin:13px;
+      margin-top:0px;
+
+      appearance:none;
+      border-width: none;
+      border-style: none;
+      border-color: none;
+      border-image: none;
+    }
+
+
+  }
+  
+  p{
+    color: #FFF;
+    font-family: Lato;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration-line: underline;
+  }
+`
+
+const Conteiner = styled.div`
+  
+  display:flex;
+`
