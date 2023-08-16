0ae621436c6d01d1c1a6fb07b7777295b0b0acf7
gustavohubnerc <hubnergc@gmail.com>
2023-08-16 11:13:02 -0300

    responsivity sign up page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -45,10 +45,8 @@ export default function SignUpPage() {
   return (
     <Conteiner>
       <ContainerText>
-        <div>
           <h1>linkr</h1>
           <h2>save, share and discover the best links on the web</h2>
-        </div>
       </ContainerText>
       <ContainerForm>
         <form onSubmit={handleSignUp}>
@@ -90,6 +88,7 @@ const ContainerText = styled.div`
   background-color:#151515;
   height:100vh;
   display:flex;
+  flex-direction:column;
   align-items: center;
   justify-content: center;
 
@@ -164,7 +163,7 @@ const ContainerForm = styled.div`
       line-height: normal;
 
       margin:13px;
-      margin-top:7px;
+      margin-top:0px;
 
       appearance:none;
       border-width: none;
@@ -188,6 +187,73 @@ const ContainerForm = styled.div`
 `
 
 const Conteiner = styled.div`
-  
   display:flex;
+
+  @media (max-width: 390px) {
+    display:flex;
+    flex-direction:column;
+    
+    width:auto;
+
+
+    div:first-child{
+      width: auto;
+      height: 26%;
+      flex-shrink: 0;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+
+      h1{
+        width:167px;
+        margin-top:10px;
+
+        color: #FFF;
+        font-family: Passion One;
+        font-size: 76px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: 3.8px;
+      }
+      h2{
+        width:250px;
+        margin-bottom:27px;
+
+        color: #FFF;
+        text-align: center;
+        font-family: Oswald;
+        font-size: 23px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+      }
+    }
+
+    div:last-child{
+      width: 390px;
+      height:621px;
+
+      padding-top:35px;
+
+      display:flex;
+      align-items: center;
+      justify-content: flex-start;
+      form{
+        
+        input{
+          width: 330px;
+          height: 55px;
+          flex-shrink: 0;
+        }
+        button{
+          margin-top:9px;
+          
+          width: 345px;
+          height: 55px;
+          flex-shrink: 0;
+        }
+      }
+    }
+  }
 `
