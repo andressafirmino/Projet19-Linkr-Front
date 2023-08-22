57516f22be9daf537b8cb7511b9b3b88b1e0328c
Caio Neme <caiocesarneme@gmail.com>
2023-08-22 18:23:21 -0300

    Style(Post): link post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -23,6 +23,17 @@ function Posts({ post }) {
 
   const textareaRef = useRef(null);
 
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -124,16 +135,7 @@ function Posts({ post }) {
       });
   };
 
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
+
 
   useEffect(() => {
     fetchPosts();
@@ -496,6 +498,4 @@ const ModalContent = styled.div`
     margin-right: 10px;
   }
 `;
-
-
 
